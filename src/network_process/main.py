4ea4204cc5b788823b28affdae0958e29c042e53
Matteo Medioli <31959430+matteomedioli@users.noreply.github.com>
2020-04-02 21:41:59 +0200

    add comment

--- src/network_process/main.py
+++ src/network_process/main.py
@@ -1,4 +1,8 @@
 from temporal_network import TemporalNetworkWrapper
 
+# First run: specify source file rel.rating tn = TemporalNetworkWrapper("network.json", "ral.rating")
+# if .json doesn't alredy exist in sources dir.
 tn = TemporalNetworkWrapper("network.json")
+
+# Example to teneto
 tn = tn.to_teneto()
